64544cb22b28512ee2321bc17fade2aaa5a3b564
cyclnxd <herakles13579@gmail.com>
2023-02-27 09:58:25 +0300

    added api controller base methods

--- backend/auth-service/src/main.py
+++ backend/auth-service/src/main.py
@@ -1,15 +1,40 @@
 import fastapi
-from cassandra.cluster import Cluster
+from dto.login.user_login_request_dto import UserLoginDTO
+from services.user_service import UserService
 
-cluster = Cluster()
 
-session = cluster.connect()
+def main():
+    
+    user_service:UserService = UserService()
 
-app = fastapi.FastAPI()
+    app = fastapi.FastAPI()
 
-@app.get("/")
-def read_root(user_id: int):
-    row = session.execute(f"SELECT * FROM store.shopping_cart WHERE userid='{user_id}'")
-    user = row.one()
-    return {"userId": str(user.userid), "itemCount": user.item_count, 'timestamp': str(user.last_update_timestamp)}
+    @app.post("/auth/login", status_code=200)
+    async def login(userDTO:UserLoginDTO):
+        #todo: validate user
+        #todo: return jwt token
+        pass
+    
+    @app.post("/auth/register", status_code=201)
+    async def register(userDTO:UserLoginDTO):
+        #todo: validate user
+        #todo: create user
+        #todo: return jwt token
+        pass
+    
+    @app.post("/auth/verify", status_code=200)
+    async def verify(token:str):
+        #todo: validate token
+        #todo: return token or success
+        pass
+    
+    @app.post("/auth/logout", status_code=200)
+    async def logout(token:str):
+        #todo: validate token
+        #todo: delete token
+        #todo: return success
+        pass
 
+    
+if __name__ == "__main__":
+    main()
