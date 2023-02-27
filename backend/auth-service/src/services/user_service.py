152ccda8d53ba53454f2374ec5df1bf727e0e91d
ShogunSkinoski <whitebirdsdk@gmail.com>
2023-02-27 21:49:55 +0300

    service update - create_user functions

--- backend/auth-service/src/services/user_service.py
+++ backend/auth-service/src/services/user_service.py
@@ -1,3 +1,5 @@
+import datetime
+import hashlib
 from models.user_login_model import UserLoginModel
 from repos.user_repo import UserRepository
 from models.user_model import UserModel
@@ -26,8 +28,41 @@ class UserService():
         return self.user_repository.get_all()
     
     def create_user(self, user):
-        return self.user_repository.create(user)
+        try:
+            user_model = self.create_user_model(user)
+            self.user_repository.create(user_model,
+                                        UserModel,
+                                        lambda x: x.username == user.username)
+            login_model = self.create_user_login_model(user, user_model.uid)
+            self.user_repository.create(login_model,
+                                        UserLoginModel,
+                                        lambda x: x.uid == user_model.uid)
+            return login_model
+        except Exception as e:
+            raise e
+
     
+    def create_user_model(self, user):
+        return UserModel(
+            username = user.username,
+            email = user.email,
+            firstName = user.firstName,
+            lastName = user.lastName,
+            is_active = user.is_active,
+            is_superuser = user.is_superuser,
+            created_at = datetime.datetime.now(),
+            updated_at = datetime.datetime.now(),          
+        )
+
+    def create_user_login_model(self, user, uid):
+        return UserLoginModel(
+            uid = uid,
+            passwordSalt = user.password,
+            passwordHash = hashlib.sha256(user.password.encode()).hexdigest(),
+            created_at = datetime.datetime.now(),
+            updated_at = datetime.datetime.now(),          
+        )
+
     def update_user(self, user):
         return self.user_repository.update(user)
     
