0e6634d676649e5b06b39dcfe37925b47996947e
cyclnxd <herakles13579@gmail.com>
2023-02-28 22:06:35 +0300

    added test modules

---
+++ backend/auth-service/src/test_main.py
@@ -0,0 +1,34 @@
+from fastapi.testclient import TestClient
+from src.main import *
+
+
+client = TestClient(app)
+
+
+# def test_read_main():
+#   response = client.get("/")
+#   assert response.status_code == 200
+#   assert response.json() == {"message": "Service is running..."}
+
+# def test_login():
+#   response = client.post("/auth/login", json={"username": "test", "password": "te33st", "email": "test@gmail.com"})
+#   if response.status_code == 200: 
+#     assert response.json() == {}
+#   else:
+#     assert response.status_code == 401 
+
+#   print(response.status_code)
+
+# def test_register():
+#   response = client.post("/auth/register", json={"username": "test", "password": "test", "email": "test@gmail.com", "firstName": "test", "lastName": "test", "role": "test", "is_active": True, "is_superuser": True})
+#   assert response.status_code != 201
+  
+# def test_register_conflict():
+#   response = client.post("/auth/register", json={"username": "test", "password": "test", "email": "test@gmail.com", "firstName": "test", "lastName": "test", "role": "test", "is_active": True, "is_superuser": True})
+#   assert response.status_code == 409
+  
+# def test_validation():
+#   response = client.get("/auth/verify", headers={
+#     "Authorization": "key"
+#   })
+#   assert response.status_code == 401
