fe364ca9b3868ce8ee8d5bf39cc40e6898dbd1c0
cyclnxd <herakles13579@gmail.com>
2023-03-01 22:57:42 +0300

    role functions

--- backend/auth-service/src/services/user_service.py
+++ backend/auth-service/src/services/user_service.py
@@ -1,9 +1,9 @@
 import datetime
-import hashlib
 from dto.register.user_register_request_dto import UserRegisterRequestDTO
 from models.user_login_model import UserLoginModel
 from repos.user_repo import UserRepository
 from models.user_model import UserModel
+from models.role_model import UserRoles
 from utilities.model_mapper import map_schema
 
 
@@ -32,8 +32,25 @@ class UserService():
         except Exception as e:
             raise Exception("User not found")
 
-    def get_users(self):
-        return self.user_repository.get_all()
+    def get_users(self, model):
+        return self.user_repository.get_all(model)
+
+    
+    def get_user_role_by_id(self, user_role_id):
+        try:
+            return self.user_repository.get(UserRoles, lambda x: x.uid == user_role_id)
+        except Exception as e:
+            raise Exception(e)
+
+    def get_user_role_by_name(self, role_name:str):
+        try:
+            return self.user_repository.get(UserRoles, lambda x: x.name == role_name)
+        except Exception as e:
+            raise Exception(e)
+
+    def get_all_user_roles(self):
+        return self.user_repository.get_all(UserRoles)
+    
 
     def create_user(self, user):
         try:
@@ -61,6 +78,9 @@ class UserService():
             user_model = map_schema(user, UserModel)
             user_login_model =  map_schema(user, UserLoginModel)
             user_login_model.uid = user_model.uid
+            user_role_uid = self.get_user_role_by_name("user").uid
+            admin_role_uid = self.get_user_role_by_name("admin").uid
+            user_model.role = admin_role_uid if user.is_superuser == True else user_role_uid
             return user_model, user_login_model
         except Exception as e:
             raise Exception(e)
