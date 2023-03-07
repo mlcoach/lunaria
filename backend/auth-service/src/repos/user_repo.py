0939be690d8dc7d85f5ee29aa221dba556433d15
cyclnxd <herakles13579@gmail.com>
2023-03-07 22:51:24 +0300

    created ErrorModel and ErrorEnum, implemented all

--- backend/auth-service/src/repos/user_repo.py
+++ backend/auth-service/src/repos/user_repo.py
@@ -1,4 +1,6 @@
 import datetime
+from models.error_model import ErrorModel
+from constants.error_enum import ErrorEnum
 
 class UserRepository():
     def __init__(self, session):
@@ -10,7 +12,7 @@ class UserRepository():
             response = list(filter(filterValue, responses))
             if response:
                 return response[0]
-            raise Exception("Record not found")
+            raise ErrorModel(ErrorEnum.RECORD_NOT_FOUND, ErrorEnum.RECORD_NOT_FOUND.value)
         except Exception as e:
             raise e
         
@@ -21,9 +23,9 @@ class UserRepository():
         try:
             response = self.get(model, filterValue)
             if response:
-                raise Exception("Record already exists")
-        except Exception as e:
-            if "Record not found" in str(e):
+                raise ErrorModel(ErrorEnum.USER_ALREADY_EXISTS, ErrorEnum.USER_ALREADY_EXISTS.value)
+        except ErrorModel as e:
+            if e.status_code == ErrorEnum.RECORD_NOT_FOUND.value:
                 user.created_at = datetime.datetime.now()
                 user.updated_at = datetime.datetime.now()
                 user.save()
@@ -36,7 +38,7 @@ class UserRepository():
             model.objects(uid=user_id).update(**arg_dict)
         except Exception as e:
             if ""'uid'"" in str(e):
-                raise Exception("Record not found")
+                raise ErrorModel(ErrorEnum.RECORD_NOT_FOUND, ErrorEnum.RECORD_NOT_FOUND.value)
         
     def delete(self, user_id):
         return self.session.execute("DELETE FROM users WHERE id = %s", (user_id,))
